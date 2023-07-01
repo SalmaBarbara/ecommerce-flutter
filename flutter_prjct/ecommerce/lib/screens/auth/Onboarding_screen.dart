ca0541c389c9ca93fae836069fcfe64b60432464
BARBARA Salma <salmabarbara1108@gmail.com>
2023-07-01 13:52:56 +0200

    styling onboarding screen and signup screen

--- flutter_prjct/ecommerce/lib/screens/auth/Onboarding_screen.dart
+++ flutter_prjct/ecommerce/lib/screens/auth/Onboarding_screen.dart
@@ -5,51 +5,118 @@ class Onboarding_screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-     
-      Text('TOKOTO '),
-    
-      Row(child :[
-      Text('Weclome to'),
-
-      Text('TOKOTO'),
-      Text('Let\'s shop!');
-
-      ]),
-     
-      Container(
-        height:500,
-        width:500 ,
-        color: Colors.black,
-      ),
-         
-     
-      Row(
-        children: [
-          Container(
-            width: 14,
-            height: 14, 
-            color:  Colors.red,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(35),
+          child: SingleChildScrollView(
+            child: Column(children: [
+               
+            Text('TOKOTO ',style: TextStyle(
+              color: Colors.orange,
+              fontWeight: FontWeight.bold,
+              fontSize: 22,
+              
+            ),
+                
+            ),
+            SizedBox(height: 10,),
+              
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children :[
+            Text('Weclome to',style: TextStyle( 
+                      color:Colors.grey,
+                      
+                      fontWeight: FontWeight.w400,
+                    ),
+                    ),
+                
+            Text('TOKOTO',
+            style:TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            )),
+            Text('Let\'s shop!',style: TextStyle( 
+                      color:Colors.grey,
+                      
+                      fontWeight: FontWeight.w400,
+                    ),),
+                  
+                
+            ]),
+            SizedBox(height: 34,),
+               
+            Container(
+              height:500,
+              width:500 ,
+              color: Colors.black,
+            ),
+             SizedBox(
+              height: 30,
+             ),  
+               
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  
+                  padding: EdgeInsets.symmetric(
+                    vertical: 4,
+                    horizontal: 12,
+                  ),
+                  decoration: BoxDecoration(
+                    shape:BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.orange,
+          
+                                ),
+                ),
+                 SizedBox(
+              width: 4,
+             ),  
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey.shade300,
+                  ),
+                ),
+                 SizedBox(
+              width: 4,
+             ),  
+                 Container(
+                 padding: EdgeInsets.all(5), 
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey.shade300,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 80,
+            ),
+                 TextButton(
+                        onPressed: printSalam,
+                        child: Text('Continue'),
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.orange,
+                          foregroundColor: Colors.white,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 130,
+                            vertical: 4,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                      ),
+                      ),
+            
+            ]
+            
+            ),
           ),
-          Container(
-            width: 14,
-            height: 14, 
-            color:  Colors.green,
-          ),
-          Container(
-            width: 14,
-            height: 14, 
-            color:  Colors.blue,
-          ),
-        ],
-      ),
-       TextButton(
-        onPressed: printSalam,
-        child: Text('Continue'),
-      ),
-      
-      ]
-      
+        ),
       ),
     );
   
